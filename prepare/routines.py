7ea8d58e98ab0cc12112a6db712773375f3c08a4
Yilun Guan <zoom.aaron@gmail.com>
2018-10-27 02:00:37 +0000

    Add FairSample routine

--- prepare/routines.py
+++ prepare/routines.py
@@ -33,9 +33,11 @@ class CleanTOD(Routine):
         
 
 class GetDataLabel(Routine):
-    def __init__(self, tod_container, downsample=None):
+    def __init__(self, tod_container, data_container, label_container, downsample=None):
         self._tod_container = tod_container
         self._downsample = downsample
+        self._data_container = data_container
+        self._label_container = label_container
         self.labels = None
         
     def initialize(self):
@@ -62,7 +64,49 @@ class GetDataLabel(Routine):
         # label: (1056, 1)
         
         # output the data and label
-        self.get_store().set("data", data)
-        self.get_store().set("label", label)
+        self.get_store().set(self._data_container, data)
+        self.get_store().set(self._label_container, label)
+
+        
+class FairSample(Routine):
+    def __init__(self, data_container, label_container):
+        """Sample good and bad detectors with equal probability for training"""
+        self._data_container = data_container
+        self._label_container = label_container
+
+    def execute(self):
+        # retrieve data and label from data store
+        data = self.get_store().get(self._data_container)
+        label = self.get_store().get(self._label_container)
+
+        # get good and bad detectors
+        n_dets = len(label)
+        good_dets = np.where(label==1)[0] # i.e. [0,10,13, ...]
+        bad_dets = np.where(label==0)[0] 
+        
+        # define the ratio of sampling, with n labels to start with
+        # and with a sampling factor of 2 means that after sampling
+        # there will be 2n labels
+        sample_factor = 2
+
+        # sample equal number of good and bad detectors for training
+        good_dets_sample = np.random.choice(good_dets, sample_factor*n_dets)
+        bad_dets_sample  = np.random.choice(bad_dets,  sample_factor*n_dets)
+
+        # merge the two set of indices and shuffle them
+        dets_sample = np.hstack([good_dets_sample, bad_dets_sample])
+        np.random.shuffle(dets_sample)
+        print "dets_sample.shape", dets_sample.shape
+        
+        # recompose data and label with sampled indices
+        new_data = data[dets_sample, :]
+        new_label = label[dets_sample]
+
+        self.get_store().set(self._data_container, new_data)
+        self.get_store().set(self._label_container, new_label)
+
+
+        
+        
 
 
--- simple-cnn.py
+++ simple-cnn.py
@@ -1,5 +1,5 @@
 from todloop.tod import TODLoader, TODInfoLoader
-from prepare.routines import CleanTOD, GetDataLabel
+from prepare.routines import CleanTOD, GetDataLabel, FairSample
 from models.simplecnn import TrainCNNModel
 from todloop.base import TODLoop
 
@@ -8,6 +8,9 @@ loop = TODLoop()
 loop.add_tod_list("data/2016_ar3_train.txt")
 loop.add_routine(TODLoader(output_key="tod_data"))
 loop.add_routine(CleanTOD(tod_container="tod_data", output_container="tod_data"))
-loop.add_routine(GetDataLabel(tod_container="tod_data", downsample=20))
+loop.add_routine(GetDataLabel(tod_container="tod_data", downsample=20, \
+                              data_container="data", label_container="label"))
+loop.add_routine(FairSample(data_container="data", label_container="label"))
+
 loop.add_routine(TrainCNNModel())
 loop.run(0, 8)
