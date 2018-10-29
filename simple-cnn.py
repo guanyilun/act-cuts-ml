8cfbc8b2ca0e0de52ed422b23e8acb3f929254c2
Yilun Guan <zoom.aaron@gmail.com>
2018-10-29 17:51:28 +0000

    Add global fair sample

--- simple-cnn.py
+++ simple-cnn.py
@@ -1,6 +1,6 @@
 from todloop.tod import TODLoader
 from prepare.routines import CleanTOD
-from training.routines import GetTrainingDataLabel, FairSample
+from training.routines import GetTrainingDataLabel, GlobalFairSample
 from training.simplecnn import TrainCNNModel
 
 from todloop.base import TODLoop
@@ -12,6 +12,6 @@ loop.add_routine(TODLoader(output_key="tod_data"))
 loop.add_routine(CleanTOD(tod_container="tod_data", output_container="tod_data"))
 loop.add_routine(GetTrainingDataLabel(tod_container="tod_data", downsample=20, \
                                       data_container="data", label_container="label"))
-loop.add_routine(FairSample(data_container="data", label_container="label"))
-loop.add_routine(TrainCNNModel(output_file="1026.h5"))
+loop.add_routine(GlobalFairSample(data_container="data", label_container="label"))
+loop.add_routine(TrainCNNModel(output_file="1029_globalfairsample.h5"))
 loop.run(0, 80)
--- training/routines.py
+++ training/routines.py
@@ -1,6 +1,7 @@
 from todloop.base import Routine
 import numpy as np
 
+
 class GetTrainingDataLabel(Routine):
     def __init__(self, tod_container, data_container, label_container, downsample=None):
         self._tod_container = tod_container
@@ -76,3 +77,65 @@ class FairSample(Routine):
 
         self.get_store().set(self._data_container, new_data)
         self.get_store().set(self._label_container, new_label)
+
+
+class GlobalFairSample(Routine):
+    def __init__(self, data_container, label_container):
+        """Sample good and bad detectors with equal probability for training"""
+        self._data_container = data_container
+        self._label_container = label_container
+        self._det_counter = 0
+        self._dets_sample = None
+
+    def initialize(self):
+        # load training label
+        label = np.load("data/train_label.npy")
+        label = np.hstack([label[:,i] for i in range(label.shape[1])])
+        
+        # get good and bad detectors
+        n_dets = len(label)
+        good_dets = np.where(label==1)[0] # i.e. [0,10,13, ...]
+        bad_dets = np.where(label==0)[0]
+
+        print "n_dets", n_dets
+
+        # randomly sample good and bad detectors equally
+        sample_factor = 1
+        good_dets_sample = np.random.choice(good_dets, sample_factor*n_dets)
+        bad_dets_sample  = np.random.choice(bad_dets,  sample_factor*n_dets)
+
+        dets_sample = np.hstack([good_dets_sample, bad_dets_sample])
+        np.random.shuffle(dets_sample)
+        print "dets_sample.shape", dets_sample.shape
+        
+        # make it accessible by execute function
+        self._dets_sample = dets_sample
+
+    def execute(self):
+        # retrieve data and label from data store
+        data = self.get_store().get(self._data_container)
+        label = self.get_store().get(self._label_container)
+        n_dets = len(label)
+
+        # get the range of dets in this TOD
+        det_lower_lim = self._det_counter
+        det_upper_lim = det_lower_lim + n_dets
+        self._det_counter += n_dets  # update counter
+        
+        dets_sample = np.array([i for i in np.arange(det_lower_lim, det_upper_lim) if i in self._dets_sample])
+        
+        # if sample list is empty, skip the TOD
+        if len(dets_sample)==0:
+            self.veto()
+            return
+        
+        np.random.shuffle(dets_sample)
+        print "dets_sample.shape", dets_sample.shape
+        
+        # recompose data and label with sampled indices
+        new_data = data[dets_sample, :]
+        new_label = label[dets_sample]
+
+        self.get_store().set(self._data_container, new_data)
+        self.get_store().set(self._label_container, new_label)
+        
